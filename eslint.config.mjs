9f3553976925679b273f942ba682a1f74527d343
devisonleave <rooklychess@gmail.com>
2026-01-03 13:24:58 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,7 @@ const eslintConfig = [
     },
     rules: {
       ...nextPlugin.configs.recommended.rules,
-      ...nextPlugin.configs['core-web-vitals'].rules,
+      ...nextPlugin.configs['core-web-vitals'].rules, 
       'react/no-unescaped-entities': 'off',
       '@next/next/no-img-element': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
